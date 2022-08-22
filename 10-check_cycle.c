917803d42d7d5779b1fa32858401c21644da069a
yusufom <yusufolamilekanmuktar@gmail.com>
2022-08-22 21:03:30 +0100

    Fixed

--- 10-check_cycle.c
+++ 10-check_cycle.c
@@ -1,24 +1,42 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: singly list to check
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current;
+	listint_t *p2;
+	listint_t *prev;
 
-	if (list == NULL)
-		return (0);
-	
-	current = list->next;
-
-	while (current != NULL)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		if (current == list)
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
-		current = current->next;
+		}
 	}
 
 	return (0);
