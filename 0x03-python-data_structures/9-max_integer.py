957ac0f382c3a7ffc9780744efb395813a94b3a7
yusufom <yusufolamilekanmuktar@gmail.com>
2022-08-29 20:29:36 +0100

    data

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    """A function that returns the max integer in a list
-        
-        Args:
-        my_list: A list of integers
-
-        Returns:
-        None if list is empty, otherwise, returns max integer in list
-    """
     sorted_list = sorted(my_list, reverse = True)
     if len(my_list) == 0:
         return None
